d98baeab432f3310652c50e19e693e93ca357d38
Rhonda  Toh <rhonda.toh.e@thalesdigital.io>
2023-06-21 17:32:08 +0800

    chore: added dad jokes functionality

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import asyncio
 from dotenv import load_dotenv
 
 import controller
+import telegram_bot
 from telegram_broadcast import initiate_cat_health_subscription
 
 def main():
     load_dotenv()
     controller.initialise_endpoint()
+    telegram_bot.start_bot()
     asyncio.run(initiate_cat_health_subscription())
 
 if __name__ == '__main__':
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import requests
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
@@ -16,13 +17,24 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         message = reader.read()
     await context.bot.send_message(chat_id=update.effective_chat.id, parse_mode="HTML",text=message)
 
+async def get_joke(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    response = requests.get("https://icanhazdadjoke.com/", headers = {"Accept": "application/json"})
+    print(response.json())
+    joke = response.json()["joke"]
+    await context.bot.send_message(chat_id=update.effective_chat.id,text=joke)
+
 
 def start_bot():
     application = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    joke_handler = CommandHandler('joke', get_joke)
+    application.add_handler(joke_handler)
     
     application.run_polling()
     
 
+if __name__ == '__main__':
+    start_bot()
